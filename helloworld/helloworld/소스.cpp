4b1f5a6fd4f719dd1ab98c8a5cca669441872110
kimganghyeon <kimganghyeon6080@naver.com>
2024-01-07 09:56:37 +0900

    Add files via upload

--- helloworld/helloworld/소스.cpp
+++ helloworld/helloworld/소스.cpp
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 int main() {
+	//Hello, World! 출력
 	printf("Hello, World!\n");
 	printf("Hello, World!\n");
 
+	//printf 한개를 쓰고 enter를 친 것 처럼 쓰기
+	printf("동해물과 백두산이\n 마르고\n 닳도록");
 }
