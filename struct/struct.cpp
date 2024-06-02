33d7141e1a3054b640af692452074d61fb8917c4
kimganghyeon <kimganghyeon6080@naver.com>
2024-06-02 18:28:49 +0900

    Update struct.cpp

--- struct/struct.cpp
+++ struct/struct.cpp
@@ -2,36 +2,34 @@
 
 #include <stdio.h>
 
-int main(void)
-{
-	struct jsu
+int main(void){
+	struct jsu //구조체 선언
 	{
-		char nae[12];
-		int os, db, hab, hhab;
+		char nae[12]; // nae 문자 배열 선언
+		int os, db, hab, hhab; //int형 변수 선언
 
 	};
 
 		struct jsu st[3] = { {"데이터1", 95, 88},
 									{"데이터2",84,91},
 									{"데이터3", 86 ,75} };
-		struct jsu* p;
-		p = &st[0];
-		(p + 1)->hab = (p + 1)->os + (p + 2)->db;
-		(p + 1)->hhab = (p + 1)->hab + p->os + p->db;
+		struct jsu* p; // 구조체 jsu의 포인터 변수 p를 선언한다.
+		p = &st[0]; // p에 st 배열의 첫 번째 요소의 주소를 저장한다.
+		(p + 1)->hab = (p + 1)->os + (p + 2)->db; //hab에 p+1의 os 값과 p+2 값을 합친 값을 저장
+		(p + 1)->hhab = (p + 1)->hab + p->os + p->db;  //hhab에 st[1]의 hab과 st[0]의 os값과 db 값을 합친 값 저장
 		
-		printf("(p + 1)->hab : %d\n", (p + 1)->hab);
-		printf("(p + 1)->hhab : %d\n", (p + 1)->hhab);
+		printf("(p + 1)->hab : %d\n", (p + 1)->hab); // hab 값 출력 (159)
+		printf("(p + 1)->hhab : %d\n", (p + 1)->hhab); //hhab 값 출력 (342)
 
-		printf("=================os=========== \n");
-		printf("p ->os : %d\n", p ->os);
-		printf("(p + 0)->os : %d\n", (p + 0)->os);
-		printf("(p + 1)->os : %d\n", (p + 1)->os);
-		printf("(p + 2)->os : %d\n", (p + 2)->os);
+		printf("=================os=========== \n"); // 줄 바꿈
+		printf("p ->os : %d\n", p ->os); //os 0번째 값 출력 (95)
+		printf("(p + 0)->os : %d\n", (p + 0)->os); //os 0번째 값 출력 (95)
+		printf("(p + 1)->os : %d\n", (p + 1)->os); //os 1번째 값 출력 (84)
+		printf("(p + 2)->os : %d\n", (p + 2)->os); //os 2번째 값 출력 (86)
 
-		printf("==============db=============\n");
-		printf("p ->db : %d\n", p->db);
-		printf("(p + 0)->db : %d\n", (p + 0)->db);
-		printf("(p + 1)->db : %d\n", (p + 1)->db);
-		printf("(p + 2)->db : %d\n", (p + 2)->db);
-
+		printf("==============db=============\n"); //줄 바꿈
+		printf("p ->db : %d\n", p->db); //db 0번째 값 출력 (88)
+		printf("(p + 0)->db : %d\n", (p + 0)->db); // db 0번째 값 출력 (88)
+		printf("(p + 1)->db : %d\n", (p + 1)->db); //db 1번째 값 출력 (91)
+		printf("(p + 2)->db : %d\n", (p + 2)->db); //db 2번째 값 출력 (75)
 }
